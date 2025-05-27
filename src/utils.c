e64507d77a48cf2d23aa63cb6ee4b75a43e4e8f9
raghavshuklaofficial <108609058+raghavshuklaofficial@users.noreply.github.com>
2025-05-27 21:08:30 +0530

    updated print logo

--- src/utils.c
+++ src/utils.c
@@ -2,23 +2,18 @@
 #include "../include/tty.h"
 
 void print_logo()
-{
-
-    printk("\t  ______  ________                   _______.                ______      ________                  \n");
+{   printk("\t  ______  ________                   _______.                ______      ________                  \n");
     printk("\t |        |      |          /\       |      |  |      |     |      |    |                          \n");
     printk("\t |        |      |         /  \      |      |  |      |     |      |    |                          \n");
     printk("\t |   __   |______'        /    \     |______|  |______|     |      |     \\\\\\                    \n");
     printk("\t |     |  |       \      /______\    |         |______|     |      |           |                   \n");
     printk("\t |     |  |        \    /        \   |         |      |     |      |           |                   \n");
     printk("\t |_____|  |         \  /          \  |         |      |     |______|    _______|                   \n");
-    printk("\t                                                                                                   \n");
-    printk("\t                                                                                                   \n");
 }
 
 void about(char *version)
 {
-    printk("\n\tGraphOS - v%s - A simple 32-bit Ring 0 operating system", version);
-    printk("\n\tProvided under the GNU General Public License v3.0");
+    printk("\n\tGraphOS - v%s - A simple 32-bit Ring 0 operating system");
     printk("\n\tInspired by Terry A. Davis : TempleOS(in memoriam)");
     printk("\n\tGitHub repository: https://github.com/raghavshuklaofficial/GraphOS");
 }
